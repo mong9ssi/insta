4f0b9c88cf25d42a4f09cdaa1ff1ce6a2b3cda50
mong9ssi <mong9ssi@naver.com>
2025-06-11 11:09:20 +0900

    lv2. 회원 조회시 민감정보 노출되지 않도록 변경

--- src/main/java/com/basic/insta/domain/dto/user/UserGetDetailResponseDto.java
+++ src/main/java/com/basic/insta/domain/dto/user/UserGetDetailResponseDto.java
@@ -6,43 +6,25 @@ import java.time.LocalDateTime;
 
 public class UserGetDetailResponseDto {
     // 속성
-    private Long userId;
-    private String userEmail;
     private String userName;
     private String content;
-    private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
     // 생성자
     public UserGetDetailResponseDto(User user) {
-        this.userId = user.getUserId();
-        this.userEmail = user.getUserEmail();
         this.userName = user.getUserName();
         this.content = user.getContent();
-        this.createdAt = user.getCreatedAt();
         this.updatedAt = user.getUpdatedAt();
     }
 
     // 기능
     // 게터
-    public Long getUserId() {
-        return userId;
-    }
-
-    public String getUserEmail() {
-        return userEmail;
-    }
-
     public String getUserName() {
         return userName;
     }
 
     public String getContent() {
         return content;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
     }
 
     public LocalDateTime getUpdatedAt() {
